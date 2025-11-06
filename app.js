15f4be694bcd8cbc9a37f902e5ea0a986f88e43c
nanthakumarrengaraj7 <nanthakumartyson777@gmail.com>
2025-11-06 15:36:51 +0530

    redo the app.js file

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
